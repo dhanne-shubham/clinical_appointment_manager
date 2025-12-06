ef1ace4b7c35280baadc7d038dd914a863f644ef
dhanne-shubham <x25108166@student.ncirl.ie>
2025-12-07 00:42:27 +0530

    Update settings.py

--- clinic_appointment/settings.py
+++ clinic_appointment/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-nke()d1wo)d9a$nuy7j=w1&(kh@kg$%tgm_9xvqkcf#@p%cfb3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
